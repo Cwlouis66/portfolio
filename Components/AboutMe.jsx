179565432651aaca61aabe393fe3935d7aa74e9e
Cwlouis66 <144451639+Cwlouis66@users.noreply.github.com>
2023-12-10 09:58:01 +0800

    commit

---
+++ Components/AboutMe.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { useTransition, useState } from "react";
+import TabButton from "./TabButton.jsx";
+
+//1d
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>React</li>
+                <li>Next.js</li>
+                <li>JavaScript</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Java</li>
+                <li>Python</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Experiences",
+        id: "experiences",
+        content: (
+          <ul className="list-disc pl-2">
+            <li>ImagineHack 2023</li>
+            <li>ETH KL 2023</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Taylor&apos;s University</li>
+                <li>Tsun Jin High School</li>
+          </ul>
+        ),
+    }
+];
+//1
+const AboutMe = () => {
+    //1b
+    const[tab, setTab] = useState("skills");
+    const[isPending, startTransition] = useTransition();
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+    //1a
+    return(
+        <section className="text-white grid" id="aboutMe">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:px-32 py-16 xl-gap-16 items-center">
+                <div>
+                    <img src="/Louis.png" alt="Louis" width={450} height={450} style={{ borderRadius: "25px" }}/>
+                </div>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
+                    <p className="text-base text-justify lg:text-lg">I am a dedicated data science and artificial intelligence student 
+                    with a profound passion for the transformative potential of blockchain technology. As I navigate the intricate world of data analysis and 
+                    AI applications, my fascination with blockchain stems from its decentralized nature, offering innovative solutions to data security, 
+                    transparency, and trust. Excited about the intersection of cutting-edge technologies, I am committed to exploring how the synergies between data science, AI, 
+                    and blockchain can shape the future of innovation and contribute to the evolution of industries. </p>
+                    <div className="flex flex-row mt-8">
+{/* 1e */}
+                        <TabButton 
+                        selectTab={() => handleTabChange("skills")}
+                        active= {tab === "skills"}
+                        >
+                            
+                            Skills
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("experiences")}
+                        active= {tab === "experiences"}
+                        >
+                            {" "}
+                            Experiences{" "}
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("education")}
+                        active= {tab === "education"}
+                        >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                        
+                    </div>
+                    <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+};
+export default AboutMe;
